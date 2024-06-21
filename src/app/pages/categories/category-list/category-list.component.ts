fb7633f65098f93772dfe720f2eb5acf2bacbbee
Wesley Costa <wesley_costa@outlook.com>
2024-06-21 19:56:34 -0300

    feat: list category

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface categoryModel {
+  id: string;
+  name: string;
+}
+
+const ELEMENT_DATA: categoryModel[] = [
+  {
+    id: '1010',
+    name: 'Premimum',
+  },
+  {
+    id: '1010',
+    name: 'Master',
+  },
+];
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.scss'
 })
 export class CategoryListComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'actions'];
+  dataSource = ELEMENT_DATA;
+
   constructor() { }
 
   ngOnInit(): void {
